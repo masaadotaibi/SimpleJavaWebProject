29b8c0eb5f48c74a3e44ee0a4e8f58bc53602439
masaadotaibi <mosaed_9999@hotmail.com>
2021-09-29 14:59:13 +0300

    Initialized the project with simple user input function

---
+++ src/main/java/com/MyServlet.java
@@ -0,0 +1,61 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		PrintWriter out = response.getWriter();
+//		out.print("Hello world, Service method!");
+//	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String fname = request.getParameter("fname");
+		
+		PrintWriter out = response.getWriter();
+		out.println("\nHello world, doGet method!");
+		
+		out.println("Hi " + fname + "!");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String fname = request.getParameter("fname");
+		
+		PrintWriter out = response.getWriter();
+		out.println("Hello world, doPost method!");
+		out.println("Hello " + fname + " in Post request!");
+	}
+
+}
